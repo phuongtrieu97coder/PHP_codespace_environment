d8ab9c631d184338069c12059caca6ecc62bb649
Phuong Trieu <82598726+phuongtrieu97coder@users.noreply.github.com>
2023-02-13 16:29:23 +0000

    cmit_8:29:AM_2_13_2023

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<?php 
-$servername = 'sql9.freemysqlhosting.net';
-$user = 'sql9594270';
-$password = 'tzXCbjkiqp';
-$database= 'sql9594270';
-
-
-try{
-	$acc = 'mysql:host='.$servername.';dbname='.$database;
-	$conn = new PDO($acc,$user,$password);
- // set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	echo 'Connected successfully!!!';
- }catch(PDOException $e){
-	echo 'Connection failed: '. $e->getMessage();
- }
-	 
- $conn = null;
- 
-?>
-
-	
-
-
-	
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
